f7cedd69e7d9c5eb8de9d18b86ac698d394db908
Jon Dedman <jonathandedman@googlemail.com>
2023-09-13 23:48:45 +0200

    improve style

--- moj/lib/checkout.rb
+++ moj/lib/checkout.rb
@@ -1,23 +1,28 @@
+# frozen_string_literal: true
+
+# The Checkout class represents a shopping cart checkout system.
+# It allows you to scan items, calculate the total price, and apply discounts.
 class Checkout
   def initialize
     @cart = []
     @prices = {
-      "FR1" => 3.11,  # Fruit tea
-      "SR1" => 5.00,  # Strawberries
-      "CF1" => 11.23  # Coffee
+      'FR1' => 3.11,  # Fruit tea
+      'SR1' => 5.00,  # Strawberries
+      'CF1' => 11.23  # Coffee
     }
     @discounts = {
-      "FR1" => { type: :buy_one_get_one_free, discounted_price: 3.11 },  # Buy one, get one free for Fruit tea
+      'FR1' => { type: :buy_one_get_one_free, discounted_price: 3.11 }, # Buy one, get one free for Fruit tea
       # "CF1" => { type: :buy_two_get_one_free, discounted_price: 11.23 },  # Buy two, get one free for Coffee example
-      "SR1" => { type: :volume_discount, quantity: 3, discounted_price: 4.50 }  # Bulk discount for Strawberries
+      'SR1' => { type: :volume_discount, quantity: 3, discounted_price: 4.50 } # Bulk discount for Strawberries
     }
   end
+
   def scan(item)
     if @prices.key?(item)
-    @cart << item
-  else
-    puts "Invalid item: #{item}"
-  end
+      @cart << item
+    else
+      puts "Invalid item: #{item}"
+    end
   end
 
   def calculate_total
@@ -25,7 +30,6 @@ class Checkout
     item_counts = Hash.new(0)
     @cart.each do |item|
       item_counts[item] += 1
-
     end
     item_counts.each do |item, count|
       if @discounts.key?(item) && count.positive?
@@ -38,7 +42,6 @@ class Checkout
           discounted_items = count * discount[:discounted_price]
           total += discounted_items
         elsif discount[:type] == :volume_discount && count < discount[:quantity]
-          discounted_items = count * discount[:discounted_price]
           total += count * @prices[item]
         end
       else
@@ -50,12 +53,11 @@ class Checkout
 end
 # Example usage of Checkout class - manual tests
 checkout = Checkout.new
-checkout.scan("SR1")
-checkout.scan("SR1")
-checkout.scan("SR1")
-checkout.scan("CF1")
-checkout.scan("FR1")
-
+checkout.scan('SR1')
+checkout.scan('SR1')
+checkout.scan('SR1')
+checkout.scan('CF1')
+checkout.scan('FR1')
 
 total_price = checkout.calculate_total
 puts "Total Price: £#{total_price}"
